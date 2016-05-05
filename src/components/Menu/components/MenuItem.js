672eb1e0f33f3ea68d2c833cc78d4e2b3832d0ac
Václav Zeman <vasek@startupjobs.cz>
2016-05-05 08:25:56 +0200

    Add count number to the menu item

--- src/components/Menu/components/MenuItem.js
+++ src/components/Menu/components/MenuItem.js
@@ -1,19 +1,28 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import cx from 'classnames';
+import _ from 'lodash';
 
 const MenuItem = (props) => {
   const {
-    faClass,
+    count, // Number of items in the category (optional)
+    faClass, // Font awesome class
     link,
     title
   } = props.item;
 
   return (
-    <li className="Menu-item">
+    <li className="Menu-item" key={_.uniqueId()}>
       <Link to={link} className="Menu-item-link">
         {faClass && <i className={cx('fa', faClass)} />}
         {title}
+
+        {
+          count &&
+          <span className="Menu-item-count">
+            {count}
+          </span>
+        }
       </Link>
     </li>
   );
