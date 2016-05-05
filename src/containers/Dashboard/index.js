46b0476ce3e0da3a58d25fe45e4ce18db497c5ba
Václav Zeman <vasek@startupjobs.cz>
2016-05-05 11:01:04 +0200

    Edit chart options and colors

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -20,13 +20,14 @@ const Dashboard = (props) => {
             </h2>
             
             <Chart 
-              chartType="AreaChart" 
-              data={getAreaChartData()} 
+              chartType="ComboChart" 
+              data={getComboChartData()} 
               options={{
-                hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-                vAxis: {minValue: 0},
                 chartArea: {'width': '85%', 'height': '75%'},
-                legend: {'position': 'bottom'}
+                legend: {'position': 'bottom'},
+                seriesType: 'bars',
+                series: {5: {type: 'line'}},
+                colors: ['#12c5d6', '#40d47e']
               }}
               width={"100%"} 
               height={"300px"} 
@@ -43,7 +44,8 @@ const Dashboard = (props) => {
             <Chart 
               options={{ 
                 chartArea: {'width': '100%', 'height': '75%'},
-                legend: {'position': 'bottom'}
+                legend: {'position': 'bottom'},
+                colors: ['#12c5d6', '#40d47e', '#ec8f6e', '#ea6153', '#f6c7b6']
               }}
               chartType="PieChart" 
               data={getPieChartData()} 
@@ -55,21 +57,27 @@ const Dashboard = (props) => {
       </div>
 
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Box className="ChartBox">
             <h2 className="ChartBox-title">
-              Chart #3
+              Quick Settings
             </h2>
-
           </Box>
         </div>
 
-        <div className="col-md-9">
+        <div className="col-md-8">
           <Box className="ChartBox">
             <h2 className="ChartBox-title">
-              Chart #4
+              Most shipping to
             </h2>
 
+            <Chart 
+              chartType="GeoChart" 
+              options={{colors: ['#40d47e']}}
+              data={getGeoChartData()} 
+              width={"100%"} 
+              height={"300px"} 
+            /> 
           </Box>
         </div>
       </div>
@@ -77,9 +85,11 @@ const Dashboard = (props) => {
   );
 };
 
+// These functions are providing only mockup data
 function getAreaChartData() {
     return [
     ['Year', 'Sales', 'Expenses'],
+    ['2012', 900, 350],
     ['2013', 1000, 400],
     ['2014', 1170, 460],
     ['2015', 660, 1120],
@@ -96,6 +106,29 @@ function getPieChartData() {
       ['Laptops', 135],
       ['Vacuum cleaners', 8]
     ];
+}
+
+function getGeoChartData() {
+  return [
+    ['Country', 'Popularity'],
+    ['Germany', 200],
+    ['United States', 300],
+    ['Brazil', 400],
+    ['Canada', 500],
+    ['France', 600],
+    ['RU', 700]
+  ];
+}
+
+function getComboChartData() {
+  return [
+     ['Month', 'Sales', 'Profit'],
+     ['2015/10', 953, 335],
+     ['2015/11', 860, 401],
+     ['2015/12', 425, 100],
+     ['2016/01', 1123, 599],
+     ['2016/02', 1586, 996]
+  ];
 }
 
 export default Dashboard;
